4360268e5cf5bba362534726a0d2d6a7573c817c
vibetv <105641536+vibetv@users.noreply.github.com>
2023-05-21 02:13:33 +0700

    Update player.js

--- player.js
+++ player.js
@@ -1,42 +1,50 @@
-function playM3u8(url, license){                        
- var player = new Clappr.Player({                 
-                    source: url,
-                    parentId: '#player',
-                    preload: 'auto',
-                    autoPlay: 'true',
-                    width: '100%',
-                    height: '100%',
-                    fullscreenEnabled:'true',
-                    hideMediaControl: 'false',
-                    plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback],
-           
-                    chromecast: {
-                      appId: '9DFB77C0',
-                      media: {
-                        type: ChromecastPlugin.Movie,
-                        title: 'Player HLS - DASH',
-                      },
-                  },
+function playM3u8(url, license) {
+    var player = new Clappr.Player({
+        source: url,
+        parentId: '#player',
+        preload: 'auto',
+        autoPlay: 'true',
+        width: '100%',
+        height: '100%',
+        fullscreenEnabled: 'true',
+        hideMediaControl: 'false',
+        plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback],
 
-                    shakaConfiguration: {
-                    preferredAudioLanguage: 'en-US',
-                    drm: {
-                    servers: {
+        chromecast: {
+            appId: '9DFB77C0',
+            media: {
+                type: ChromecastPlugin.Movie,
+                title: 'Player HLS - DASH',
+            },
+        },
+
+        shakaConfiguration: {
+            preferredAudioLanguage: 'en-US',
+            drm: {
+                servers: {
                     'com.widevine.alpha': license
-                             }
-                         },
-                    streaming: {
-                      rebufferingGoal: 15
-                    }
-                  },
-                  shakaOnBeforeLoad: function(shaka_player) {
-                    // shaka_player.getNetworkingEngine().registerRequestFilter() ...
-                  },
+                }
+            },
+            streaming: {
+                rebufferingGoal: 15
+            }
+        },
+        shakaOnBeforeLoad: function(shaka_player) {
+            // shaka_player.getNetworkingEngine().registerRequestFilter() ...
+        },
+        function noServer() {
+            var player = document.getElementById('TIyS3zTF');
+            document.body.style.backgroundImage = "url('https://media.tenor.com/u7LmCHI89N8AAAAC/server-offline.gif')";
+            document.body.style.backgroundSize = "contain";
+            player.innerHTML +=
+                '<img src="https://media.tenor.com/u7LmCHI89N8AAAAC/server-offline.gif" width="100%" height="100%">';
 
-                });
+        },
 
-  document.title = "Watching: " +url;
-    
+    });
+
+    document.title = "Watching: " + url;
+
 }
 
 playM3u8(window.location.href.split("#")[1], window.location.href.split("=")[1])
