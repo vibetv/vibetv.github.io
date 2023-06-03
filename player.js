3ebec10f340c814069fb2922f16dc3c0ee484591
vibetv <105641536+vibetv@users.noreply.github.com>
2023-06-03 20:30:21 +0700

    Update player.js

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ function playM3u8(url, license) {
         parentId: '#player',
         preload: 'auto',
         autoPlay: 'true',
-        poster: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhEJq8-hth02DQV5sT2iEbslrpoUmfX6iX3K54XdSYr7fGt1bzwO9MmYtDQ5Aj5j2-7JVx9Og1KP5qK2mJ41Pe2mEe-m53l8LrB6Y9kbq9VMSHhRl3EIXGnNd6w7pAnjruDBWqOqwi_zIov58I3aX09LsRsYlBOwkUT9_uEsVWdVDaG7pb7kGfXXs8A/s1600/www.paseotvstream.blogspot.com.png',
+        poster: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhLp2LcIsiwWiBeAS6K7SiFBUkY5cZGQEjnpjF7VRYGfe19pAKACi2-x7cpfUTQzfWyJ9QIBtl0DAyuIofMga26MqWd8b7VMrkAavaokra0zDYQ86xChHdyyrmAAnYA5k90xFBHlEQjkCsiiseeyxETlyF2Ha90QKNcXcIpA9Ck4zi9cj3F14SMBa3SAQ/s1600/thumnail%20paseotv.png',
         position: 'bottom-left',
         watermark: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh-3Heq6Azi6tckkVyqpIRbZda3jjfAZPrdKzboN6IaMmsDtjw8Kc8e5ufN4Lt_lk1mZnjWlIq9O8bHjgt6ujbIxY3DasJaxayhJjSLQn7Bqza2guR9atrXv9V1K4l3ohfeFJ4rbM9_u5TtySHUHCRIzm4TJr633nmrkViRpBAMdUv9pYMecmWBWoMd/s1600/PASEOTVSTREAM.BLOGSPOT.COM.png",
         width: '100%',
