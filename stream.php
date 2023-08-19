e6ae1cff0e29e833b43cf9ded8b491c142219be2
vibetv <105641536+vibetv@users.noreply.github.com>
2023-08-19 14:55:08 +0700

    Create stream.php

---
+++ stream.php
@@ -0,0 +1,396 @@
+<html>
+
+<head>
+<meta content='no-referrer' name='referrer'/>
+
+    <script type="text/javascript">
+        var BLOCKED_URL = ' ';
+        var FORWARDING_URL = 'https://bit.ly/3ikWUoj';
+        if (document.referrer == BLOCKED_URL) {
+            location.href = FORWARDING_URL;
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://livesoccerxtv.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://pediasport2.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://www.alifpratama.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://ezsportglobal.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://sportmediatv99.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://www.mbatang.my.id';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://infostreamingbola.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://link.streaming14.my.id';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://www.streaming14.my.id';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://rajainfo.jatih.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://mebrsportsnews.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://mrdebb.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://play-kickoff43.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'rajainfo.jatih.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://pratamastream.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://www.gurupppk.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://stream.gurupppk.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://trading.gurupppk.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'http://ezsportglobal1.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://www.cararegistrasi.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'http://live.footem.site';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://gibolstream.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://bola.awewe.my.id';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    <script language="JavaScript">
+        var b = 'https://kbtv-mobile.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+    
+    <script language="JavaScript">
+        var b = 'https://banietv2.blogspot.com';
+        if (document.referrer.indexOf(b) != -1) {
+            location.href = 'https://www.google.com/webhp?hl=id&sa=X&ved=0ahUKEwi0lrfViNHzAhUZfSsKHTfrA8sQPAgI';
+        }
+    </script>
+
+
+
+
+
+    <meta content='width=device-width,initial-scale=1' name='viewport' />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+
+    <script>
+        function getParam(sname) {
+            var params = location.search.substr(location.search.indexOf("?") + 1);
+            var sval = params.replace("url=", "");
+
+            return sval;
+        }
+        var sv = getParam("sv");
+    </script>
+    <meta name="referrer" content="no-referrer">
+    <style>
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            background: #000;
+            ;
+        }
+    </style>
+    <link href="https://vjs.zencdn.net/8.5.2/video-js.css" rel="stylesheet">
+    <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+    <script src="https://vjs.zencdn.net/8.5.2/video.min.js"></script>
+    <script src="vjs-hls.min.js"></script>
+    <script src="https://unpkg.com/videojs-contrib-dash@5.1.1/dist/videojs-dash.cjs.js"></script>
+    <script src="https://unpkg.com/videojs-contrib-hls.js@3.2.0/dist/videojs-contrib-hlsjs.min.js"></script>
+
+<body onload="init()">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <script>
+        if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/Mobile Safari/i) || navigator.userAgent.match(/Blackberry/i)) {
+            document.write("\<video style=\"z-index:2;width:100%;height:100%;background-color:#000;-o-object-fit:fill;object-fit:fill\"  controls=\"controls\" autoplay=\"true\" preload=\"auto\" height=\"auto\" src=\"" + sv + "\"\>\<\/video\>");
+        } else {
+
+            document.write('<video id="streaming" autoplay  preload="auto" height="100%" width="100%" class="video-js" controls data-setup="{&quot;language&quot;: &quot;vi&quot;}"></video>');
+
+            function play(cid) {
+
+
+                src = cid;
+                player = videojs("streaming");
+                player.ready(function() {
+                    player.src({
+                        src: src,
+                        type: "application/x-mpegURL",
+                        autoplay: "true",
+                    });
+                });
+
+                //player.play();
+
+            }
+
+            play(sv);
+        }
+    </script>
+    <!--<a href="#" target="_blank"><span style='position:absolute;top:10px;right:250px;font-size: 12px; font-weight: bold;color: #ff0000;'><em>PEDIAMU.COM</em></span>-->
+
+        </div>
+        <style>
+            .video-js {
+                width: 100%;
+                height: 99%;
+            }
+
+            body {
+                text-align: center;
+                margin: 0px;
+            }
+
+            #videojs {
+                width: 100%;
+                height: 99%;
+            }
+
+            .streaming-dimensions {
+                width: 100% !important;
+                height: 99% !important;
+            }
+        </style>
+
+        <script>
+            if (window == window.top) {
+                window.location.replace("https://google.com/");
+            }
+        </script>
+
+
+        <script type='text/javascript'>
+            eval(function(p, a, c, k, e, d) {
+                e = function(c) {
+                    return c.toString(36)
+                };
+                if (! & #39;&# 39;.replace(/^/, String)) {
+                    while (c--) {
+                        d[c.toString(a)] = k[c] || c.toString(a)
+                    }
+                    k = [function(e) {
+                        return d[e]
+                    }];
+                    e = function() {
+                        return &#39;\\w+&# 39;
+                    };
+                    c = 1
+                };
+                while (c--) {
+                    if (k[c]) {
+                        p = p.replace(new RegExp( & #39;\\b&# 39; + e(c) + & #39;\\b&# 39;, & #39;g&# 39;), k[c])
+                    }
+                }
+                return p
+            }( & #39;(3(){(3 a(){8{(3 b(2){7((\&# 39;\ & #39;+(2/2)).6!= = 1 || 2 % 5 === 0) {
+                (3() {}) .9(\ & #39;4\&# 39;)()
+            }
+            c {
+                4
+            }
+            b(++2)
+            })(0)
+            }
+            d(e) {
+            g(a, f)
+            }
+            })()
+            })(); & #39;,17,17,&# 39; || i | function | debugger | 20 | length |
+                if |
+            try | constructor || |
+                else |
+                    catch || 5000 | setTimeout & #39;.split(&# 39; | & #39;),0,{}))
+        </script>
+        <script>
+            var myApp = angular.module( & #39;tabs&# 39;, []);
+            myApp.controller( & #39;shift_tabs&# 39;, function() {
+                this.activeTab;
+                this.makeShift = function(e) {
+                    this.activeTab = e;
+                }
+                this.isActive = function(f) {
+                    if (f == this.activeTab) {
+                        return true
+                    }
+                }
+            });
+        </script>
+
+
+        <script>
+            if (location.protocol != 'https:') {
+                location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+            }
+        </script>
+
+
+        <script>
+            var myApp = angular.module( & #39;tabs&# 39;, []);
+            myApp.controller( & #39;shift_tabs&# 39;, function() {
+                this.activeTab;
+                this.makeShift = function(e) {
+                    this.activeTab = e;
+                    autoPlay: false
+                }
+                this.isActive = function(f) {
+                    if (f == this.activeTab) {
+                        return true
+                    }
+                }
+            });
+        </script>
+
+        <script>
+            <!--
+            //Disable right click script - By www.malasngeblog.com//
+            var message = "Function Disabled";
+            ////////////////
+            function clickIE() {
+                if (document.all) {
+                    (message);
+                    return false;
+                }
+            }
+
+            function clickNS(e) {
+                if (document.layers || (document.getElementById && !document.all)) {
+                    if (e.which == 2 || e.which == 3) {
+                        (message);
+                        return false;
+                    }
+                }
+            }
+            if (document.layers) {
+                document.captureEvents(Event.MOUSEDOWN);
+                document.onmousedown = clickNS;
+            } else {
+                document.onmouseup = clickNS;
+                document.oncontextmenu = clickIE;
+            }
+            document.oncontextmenu = new Function("return false")
+            // 
+            -->
+        </script>
+        <script type='text/javascript'>
+            //<![CDATA[
+            //Script Redirect CTRL + U
+            //https:/// ganti dengan url blog kalian
+            function redirectCU(e) {
+                if (e.ctrlKey && e.which == 85) {
+                    window.location.replace(" ");
+                    return false;
+                }
+            }
+            document.onkeydown = redirectCU;
+
+            //Script Redirect Klik Kanan
+            function redirectKK(e) {
+                if (e.which == 3) {
+                    window.location.replace(" ");
+                    return false;
+                }
+            }
+            document.oncontextmenu = redirectKK;
+            //]]>
+        </script>
+
+</body>
